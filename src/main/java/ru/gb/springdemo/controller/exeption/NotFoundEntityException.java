d4ddc42bb71c58468edff7a4a1baa5da7466b8b3
Javokhir <maska_uz@mail.ru>
2024-06-28 20:29:42 +0300

    homework-8

--- src/main/java/ru/gb/springdemo/controller/exeption/NotFoundEntityException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.springdemo.exception;
-
-public class NotFoundEntityException extends RuntimeException {
-    public NotFoundEntityException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/gb/springdemo/controller/exeption/UserIssueLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.springdemo.exception;
-
-public class UserIssueLimitExceededException extends RuntimeException {
-    public UserIssueLimitExceededException(String message) {
-        super(message);
-    }
-}
